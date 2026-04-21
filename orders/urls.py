9fee4c5356f03e69dcebfea49e4bdcc1e29eecd4
juba-touam <jubatouam61@gmail.com>
2026-04-21 22:09:33 +0200

    feat: change orders endpoint to /api/orders

--- orders/urls.py
+++ orders/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet
 
 router = DefaultRouter()
-router.register("orders", OrderViewSet)
+router.register("api/orders", OrderViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
