dec87e3c5142b2fa17a80655342637ee8f44b5fb
elGiordano <aguelid_aksel@gmail.com>
2026-04-23 02:18:31 +0200

    fix(api): fix endpoint api orders trailing slash and add pre-push hook

--- orders/urls.py
+++ orders/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet
 
 router = DefaultRouter()
-router.register("orders", OrderViewSet)
+router.register(r'api/orders', OrderViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
